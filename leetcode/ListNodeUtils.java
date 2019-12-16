10018a1cdefce6394e550f47415c289e50995315
Sohil Ladhani <supertakumi86@gmail.com>
2019-12-16 23:07:57 +0530

    Add 1290-Convert Binary to Integer
    Also includes ListNode and related functionality

---
+++ leetcode/ListNodeUtils.java
@@ -0,0 +1,14 @@
+public class ListNodeUtils {
+
+    public static ListNode createListFromIntegerArray(int elems[]) {
+        int elemsLength = elems.length;
+        if (elemsLength <= 0) return null;
+        ListNode head = new ListNode(elems[0]);
+        ListNode dummyHead = head;
+        for (int i = 1; i < elemsLength; i++) {
+            head.next = new ListNode(elems[i]); 
+            head = head.next;
+        }
+        return dummyHead;
+    }
+}

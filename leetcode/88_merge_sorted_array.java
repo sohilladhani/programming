de0cf62f3c5cacf8a491fdf27d462417a83884d5
Sohil Ladhani <supertakumi86@gmail.com>
2020-01-11 21:14:38 +0530

    Update to utility method from Utils

--- leetcode/88_merge_sorted_array.java
+++ leetcode/88_merge_sorted_array.java
@@ -49,23 +49,6 @@ class MergeSortedArray {
         int m = 3;
         int n = 3;
         msa.merge(nums1, m, nums2, n);
-        System.out.println(integerArrayToString(nums1));
-    }
-
-    public static String integerArrayToString(int[] nums, int length) {
-        if (length == 0) {
-            return "[]";
-        }
-    
-        String result = "";
-        for(int index = 0; index < length; index++) {
-            int number = nums[index];
-            result += Integer.toString(number) + ", ";
-        }
-        return "[" + result.substring(0, result.length() - 2) + "]";
-    }
-    
-    public static String integerArrayToString(int[] nums) {
-        return integerArrayToString(nums, nums.length);
+        System.out.println(Utils.integerArrayToString(nums1));
     }
 }

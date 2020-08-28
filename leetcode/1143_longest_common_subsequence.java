e31f0b95633819d8161165dd503f915ca2f4a9a2
Sohil Ladhani <supertakumi86@gmail.com>
2020-08-28 21:01:47 +0530

    Recursion with memoization

--- leetcode/1143_longest_common_subsequence.java
+++ leetcode/1143_longest_common_subsequence.java
@@ -38,24 +38,30 @@ Constraints:
 */
 
 class LongestCommonSubsequence {
+    int[][] cache;
     public int longestCommonSubsequence(String text1, String text2) {
+        cache = new int[text1.length() + 1][text2.length() + 1];
         return lcs(text1, text2, text1.length(), text2.length()); 
     }
     
     private int lcs(String X, String Y, int n, int m) {
         if(n == 0 || m == 0) return 0;
+        if(cache[n][m] != 0) return cache[n][m];
         
         if(X.charAt(n - 1) == Y.charAt(m - 1)) {
-            return (1 + lcs(X, Y, n-1, m-1));
+            return cache[n][m] = (1 + lcs(X, Y, n-1, m-1));
         } else {
-            return Math.max(lcs(X, Y, n, m-1), lcs(X, Y, n-1, m));
+            return cache[n][m] = Math.max(lcs(X, Y, n, m-1), lcs(X, Y, n-1, m));
         }
     }
 
     public static void main(String[] args) {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
-        String X = "abcde";
-        String Y = "ace";
+        String X = "mhunuzqrkzsnidwbun";
+        String Y = "szulspmhwpazoxijwbq";
+        long startTime = System.nanoTime();
         System.out.println(lcs.longestCommonSubsequence(X, Y));
+        long endTime = System.nanoTime();
+        System.out.println("Time: " + ((endTime - startTime)/1000_000) + " ms");
     }
 }

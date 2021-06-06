598db594dcfff0b035dd7c1537c266289ee72f6c
Sohil Ladhani <supertakumi86@gmail.com>
2021-06-06 21:53:23 +0530

    Update code and solve the problem using Union Find
    https://binarysearch.com/problems/Friend-Groups

--- 100daysofcode/FriendGroups.java
+++ 100daysofcode/FriendGroups.java
@@ -1,27 +1,37 @@
 /* https://binarysearch.com/problems/Friend-Groups */
 
+import java.util.*;
+
 class Solution {
-    boolean[] visited;
+    int[] parent;
     public int solve(int[][] friends) {
-        if(friends.length <= 1) return friends.length;
-        int connected = 0;
-        visited = new boolean[friends.length];
+        parent = new int[friends.length];
+        int groups = 0;
+        Arrays.fill(parent, -1);
         for(int i = 0; i < friends.length; i++) {
-            if(visited[i] == false) {
-                connected++;
-                for(int friend: friends[i]) {
-                    dfs(friends, friend);
+            int a = find(i);
+            for(int j = 0; j < friends[i].length; j++) {
+                int b = find(friends[i][j]);
+                if(a != b) {
+                    union(a, b);
                 }
             }
         }
-        return connected;
+        for(int i = 0; i < parent.length; i++) {
+            if(parent[i] < 1) groups++;
+        }
+        return groups;
     }
 
-    private void dfs(int[][] friends, int friend) {
-        if(visited[friend] == true) return;
-        visited[friend] = true;
-        for(int friendOfFriend: friends[friend]) {
-            dfs(friends, friendOfFriend);
+    private int find(int n) {
+        while(parent[n] > 0) {
+            n = parent[n];
         }
+        return n;
+    }
+
+    private void union(int a, int b) {
+        parent[a] = parent[a] + parent[b];
+        parent[b] = a;
     }
 }
